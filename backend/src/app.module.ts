f5d8ba46d0a8e8387cd393c0ae3883f3fd492eca
zy1p <zy1p@icloud.com>
2025-07-05 05:28:56 +0000

    chore: setup pino logger

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
+import { EnvModule } from '@lib/env';
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EnvModule } from '@lib/env';
 
 @Module({
-  imports: [EnvModule],
+  imports: [
+    EnvModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        // install 'pino-pretty' package in order to use the following option
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV, Env } from '@lib/env';
+import { Logger } from 'nestjs-pino';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
 
   const env = app.get<Env>(ENV);
+  const logger = app.get(Logger);
+  app.useLogger(logger);
 
-  await app.listen(env.PORT);
+  await app.listen(env.PORT, '0.0.0.0', async () => {
+    const url = await app.getUrl();
+    logger.log(`Server listening at ${url}`);
+  });
 }
 void bootstrap();
