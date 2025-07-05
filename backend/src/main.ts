9f03a631a30f7ca9768c84e374abdf1536408e8f
zy1p <zy1p@icloud.com>
2025-07-05 05:31:32 +0000

    chore: setup openapi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ENV, Env } from '@lib/env';
 import { Logger } from 'nestjs-pino';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -13,9 +14,25 @@ async function bootstrap() {
   const logger = app.get(Logger);
   app.useLogger(logger);
 
+  const SWAGGER_PATH = 'docs';
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('API document')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_PATH, app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      // docExpansion: 'none'
+    },
+  });
+
   await app.listen(env.PORT, '0.0.0.0', async () => {
     const url = await app.getUrl();
     logger.log(`Server listening at ${url}`);
+    logger.log(`Documentation at ${url}/${SWAGGER_PATH}`);
   });
 }
 void bootstrap();
