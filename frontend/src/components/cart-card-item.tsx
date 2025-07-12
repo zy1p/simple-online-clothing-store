36bb0f30dcde3f549150b5cfd92f36a0d11c44db
zy1p <zy1p@icloud.com>
2025-07-12 23:48:51 +0000

    feat: implement cart item component and enhance cart display logic

---
+++ frontend/src/components/cart-card-item.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { useCartStore } from "@/stores/cart-store";
+import { Skeleton } from "./ui/skeleton";
+
+import products from "@/../public/products.json";
+import { Delete } from "lucide-react";
+import ImageWithFallback from "./image-with-fallback";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
+type CartItemProps = {
+  _id: string;
+  quantity: number;
+};
+
+export function CartCardItem({ _id, quantity }: CartItemProps) {
+  const product = products.find((item) => item._id === _id);
+
+  if (product)
+    return (
+      <Card className="mx-auto w-full max-w-2xl">
+        <CardContent className="flex flex-col items-center gap-4 md:flex-row">
+          <ImageWithFallback
+            src={product.imageUrl}
+            alt={product.name}
+            height={200}
+            width={200}
+            className="size-50"
+          />
+
+          <div className="w-full max-w-128 space-y-2">
+            <CardTitle>{product.name}</CardTitle>
+            <CardDescription className="h-16">
+              {product.description}
+            </CardDescription>
+
+            <div className="flex justify-between">
+              <div className="text-lg font-semibold">${product.price}</div>
+              <div className="text-muted-foreground text-sm">
+                Quantity: {quantity}
+              </div>
+            </div>
+
+            <CardAction className="justify-end">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    size={"icon"}
+                    onClick={() => {
+                      const { removeItem } = useCartStore.getState();
+                      removeItem(_id);
+                    }}
+                  >
+                    <Delete />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Remove from cart</p>
+                </TooltipContent>
+              </Tooltip>
+            </CardAction>
+          </div>
+        </CardContent>
+      </Card>
+    );
+
+  return <Skeleton className="h-60 max-w-full" />;
+}
--- frontend/src/components/cart-card.tsx
+++ frontend/src/components/cart-card.tsx
@@ -5,12 +5,14 @@ import {
   CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { useCartStore } from "@/stores/cart-store";
+import Link from "next/link";
 import { useEffect, useState } from "react";
+import { CartCardItem } from "./cart-card-item";
+import { Skeleton } from "./ui/skeleton";
 
 export function CartCard() {
   const { clearCart, getItemsCount } = useCartStore();
@@ -21,23 +23,64 @@ export function CartCard() {
   }, []);
 
   return (
-    <Card>
+    <Card className="mx-auto max-w-4xl">
       <CardHeader>
         <CardTitle>Cart</CardTitle>
-        <CardDescription>Items in your cart</CardDescription>
+        <CardDescription>
+          {isClient && useCartStore.persist.hasHydrated() ? (
+            getItemsCount() > 0 ? (
+              <p>You have {getItemsCount()} items in your cart.</p>
+            ) : (
+              <p>Your cart is empty.</p>
+            )
+          ) : (
+            <Skeleton className="h-5 max-w-[200px]" />
+          )}
+        </CardDescription>
+
+        <CardAction className="justify-end space-x-4">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            className="w-24"
+            onClick={clearCart}
+          >
+            Clear Cart
+          </Button>
+
+          <Button size={"sm"} className="w-24">
+            Check Out
+          </Button>
+        </CardAction>
       </CardHeader>
       <CardContent>
-        {isClient && getItemsCount() > 0 ? (
-          <p>You have {getItemsCount()} items in your cart.</p>
-        ) : (
-          <p>Your cart is empty.</p>
-        )}
+        <div className="flex flex-col gap-4">
+          {isClient && useCartStore.persist.hasHydrated() ? (
+            getItemsCount() > 0 ? (
+              Object.entries(useCartStore.getState().items).map(
+                ([id, quantity]) => (
+                  <CartCardItem key={id} _id={id} quantity={quantity} />
+                ),
+              )
+            ) : (
+              <p>
+                Explore our{" "}
+                <Link
+                  href="/catalogue"
+                  className="text-foreground/90 hover:text-foreground underline"
+                >
+                  catalogue
+                </Link>{" "}
+                for more products.
+              </p>
+            )
+          ) : (
+            Array.from({ length: 3 }).map((_, index) => (
+              <Skeleton key={index} className="mx-auto h-60 w-full max-w-2xl" />
+            ))
+          )}
+        </div>
       </CardContent>
-      <CardFooter>
-        <CardAction className="justify-end">
-          <Button onClick={clearCart}>Clear Cart</Button>
-        </CardAction>
-      </CardFooter>
     </Card>
   );
 }
